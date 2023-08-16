37d7312798e25fc98cd0a353c9f3dfbc0e2678e0
amekusa <hello@amekusa.com>
2023-08-17 01:04:14 +0900

    Update version to 2.0.0

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ import cp from 'node:child_process';
 import semver from 'semver';
 import { Command } from 'commander';
 
-const version = '1.2.1';
+const version = '2.0.0';
 
 let options = {
 	dryRun: false,
